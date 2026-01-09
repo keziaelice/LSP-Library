cc66ba32b40c9aa1f1a5dd2ffc6b15fce2ea0f4a
keziaelice <127367044+keziaelice@users.noreply.github.com>
2026-01-09 22:53:04 +0700

    Initial Commit

---
+++ LSP-Library/LSP_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LSP_LibraryApp.swift
+//  LSP-Library
+//
+//  Created by Kezia Elice on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LSP_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LSP-LibraryTests/LSP_LibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  LSP_LibraryTests.swift
+//  LSP-LibraryTests
+//
+//  Created by Kezia Elice on 09/01/26.
+//
+
+import Testing
+@testable import LSP_Library
+
+struct LSP_LibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
